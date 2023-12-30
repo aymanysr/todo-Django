015ffa20352bceeb66b2265e3624883d811b1358
aimane yousr <aimaneyousr@gmail.com>
2023-12-30 16:45:34 +0100

    rm field shared_link from todolist/rm field user from todos/add field owner to todolist/create model SharedTodoList/add field user to todolist

--- todo/models.py
+++ todo/models.py
@@ -3,16 +3,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class TodoList(models.Model):
-    title =models.CharField(max_length=100, null=False, blank=False)
-    shared_link = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=100, null=False, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todolist_owner", default=1)
+    user = models.ManyToManyField(User, through="SharedTodoList", related_name="todolist_user")
 
 class Todos(models.Model):
-    TodoList = models.ForeignKey(TodoList, on_delete=models.CASCADE, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200, null=False, blank=False)
     completed = models.BooleanField(default=False)
-    updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    TodoList = models.ForeignKey(TodoList, on_delete=models.CASCADE, null=True, blank=True)
+
+class SharedTodoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    TodoList = models.ForeignKey(TodoList, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
