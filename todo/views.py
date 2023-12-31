73cea6b606439486828ead60d3fac89dbeb1e4c1
aimane yousr <aimaneyousr@gmail.com>
2023-12-31 23:34:43 +0100

    implemented create_todolist & delete_todolist, also fixed create/mark/delete _todo

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from .models import Todos
+from .models import TodoList
 from . forms import CreateUserForm
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 
 from django.contrib.auth.decorators import login_required
 
@@ -40,26 +41,50 @@ def my_login(request):
 @login_required(login_url='login')
 def dashboard(request):
     todos = Todos.objects.filter(user=request.user).order_by('-id')
-    return render(request, 'todo/dashboard.html', {'todos': todos})
+    todolist = TodoList.objects.filter(owner=request.user).order_by('-id')
+    return render(request, 'todo/dashboard.html', {'todos': todos, 'todolist': todolist})
 
 def logout_user(request):
     logout(request)
     return redirect("")
 
-def index(request):
-    # todos = Todos.objects.all().order_by('-id')
-    # return render(request, 'index.html', {'todos': todos})
-    pass
+@login_required(login_url='login')
+def create_todolist(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        user = request.user
+        todolist = TodoList.objects.create(title=title, owner=user)
+        todolist.save()
+        todolist = TodoList.objects.filter(owner=request.user).order_by('-id')
+        todos = Todos.objects.filter(user=request.user, todolist__in=todolist).order_by('-id')
+        return render(request, 'todo/todo-list.html', {'todolist': todolist, 'todos': todos})
+    else:
+        return render(request, 'todo/create_todolist.html')
+
+def delete_todolist(request, pk):
+    todolist = TodoList.objects.get(pk=pk)
+    if todolist.owner != request.user:
+        return redirect('todo/todo-list.html')
+    todolist.delete()
+    todolist = TodoList.objects.filter(owner=request.user).order_by('-id')
+    todos = Todos.objects.filter(user=request.user).order_by('-id')
+    return render(request, 'todo/todo-list.html', {'todolist': todolist, 'todos': todos})
+
 
 # Creating a new Todo
 @login_required(login_url='login')
-def create_todo(request):
-    title = request.POST.get('title')
-    user = request.user
-    todo = Todos.objects.create(title=title, user=user)
-    todo.save()
-    todos = Todos.objects.filter(user=user).order_by('-id')
-    return render(request, 'todo/todo-list.html', {'todos': todos})
+def create_todo(request, pk):
+    todolist = get_object_or_404(TodoList, pk=pk)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        user = request.user
+        todo = Todos.objects.create(title=title, user=user, todolist=todolist)
+        todo.save()
+        todos = Todos.objects.filter(user=user, todolist=todolist).order_by('-id')
+        todolist = TodoList.objects.filter(owner=request.user).order_by('-id')
+        return render(request, 'todo/todo-list.html', {'todolist':todolist, 'todos': todos})
+    else:
+        return render(request, 'todo/create_todo.html', {'pk': pk})
 
 
 # Marking completed True
@@ -67,8 +92,9 @@ def mark_todo(request, pk):
     todo = Todos.objects.get(pk=pk)
     todo.completed = not todo.completed
     todo.save()
-    todos = Todos.objects.filter(user=request.user).order_by('-id')
-    return render(request, 'todo/todo-list.html', {'todos': todos})
+    todos = Todos.objects.filter(user=request.user, todolist=todo.todolist).order_by('-id')
+    todolist = TodoList.objects.filter(owner=request.user).order_by('-id')
+    return render(request, 'todo/todo-list.html', {'todolist': todolist,'todos': todos})
 
 # Deleting a Todo
 def delete_todo(request, pk):
@@ -76,5 +102,6 @@ def delete_todo(request, pk):
     if todo.user != request.user:
         return redirect('dashboard')
     todo.delete()
-    todos = Todos.objects.all().order_by('-id')
-    return render(request, 'todo/todo-list.html', {'todos': todos})
+    todos = Todos.objects.filter(user=request.user, todolist=todo.todolist).order_by('-id')
+    todolist = TodoList.objects.filter(owner=request.user).order_by('-id')
+    return render(request, 'todo/todo-list.html', {'todolist':todolist, 'todos': todos})
