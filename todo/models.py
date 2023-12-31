a778e0ff09b720ddd0f9bac25efbdf3ab82b2efe
aimane yousr <aimaneyousr@gmail.com>
2023-12-31 23:31:38 +0100

    added title to sharedtodolist & added related_name to todolist in Todos

--- todo/models.py
+++ todo/models.py
@@ -12,12 +12,13 @@ class Todos(models.Model):
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    TodoList = models.ForeignKey(TodoList, on_delete=models.CASCADE, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    todolist = models.ForeignKey(TodoList, on_delete=models.CASCADE, related_name="todos")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class SharedTodoList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    TodoList = models.ForeignKey(TodoList, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100, default="Default title")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    todolist = models.ForeignKey(TodoList, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
